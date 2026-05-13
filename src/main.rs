e236e3a0ce6717acc7b1f62560df1d9c8f3c7272
T342 <chaircomputeroverdrive@gmail.com>
2026-05-12 18:33:32 -0700

    inital commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use rand::RngExt;
+use std::cmp::Ordering;
+use std::io;
+/// this shit down here
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::rng().random_range(1..=100);
+
+    println!("The number is {}", secret_number);
+
+    println!("Put in the guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => eprintln!("You win!"),
+
+    }
+}
